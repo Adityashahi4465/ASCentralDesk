6467bd6115eb6bb54118770fbf5991866565f177
Aditya Shahi <97585273+Adityashahi4465@users.noreply.github.com>
2023-08-30 06:07:03 +0530

    Delete lib directory

--- lib/screens/services/utils.dart
+++
@@ -1,6 +0,0 @@
-import 'package:file_picker/file_picker.dart';
-
-Future<FilePickerResult?> pickImage() async {
-  final image = await FilePicker.platform.pickFiles(type: FileType.image);
-  return image;
-}
